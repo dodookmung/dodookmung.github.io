db2e845f59dc9727a13b7cc0b825db65d09b1895
dodookmung <dodookmungx@gmail.com>
2024-06-04 03:05:45 +0900

    start website

---
+++ README.md
@@ -0,0 +1,8 @@
+# Retro 98's style Web Site
+
+일단 style.css로 디자인 레이아웃을 짜고,</br>
+X - 버튼 인터렉션은 javascript로 짜는데, 일단 parcel.js 사용해서 app.js로 확인.</br>
+image 파일 로드하는 부분에서 parcel의 파싱 방식이 작동하지 않음.</br>
+</br>
+</br>
+서버사이드 렌더링을 어떻게 하는지 모르니까 node.js 배우기전까진 html 링크로 노다가 하자.
---
+++ app.js
@@ -0,0 +1,17 @@
+const container = document.getElementById('root');
+
+let template = `
+`;
+
+
+template = `
+`;
+
+
+
+//const style = document.createElement('style');
+//style.innerHTML = ``;
+//document.head.appendChild(style);
+
+
+container.innerHTML = template;
